c67955c7e2990fe2481fcd64ea940767ea3708ee
janearisah <janet@chaka.ng>
2019-11-06 23:36:54 +0100

    WIP: Started Home Page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit {
+  cities: object[] = [
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text:
+        "14 Porters in Abia with 10 deliveries14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text:
+        "14 Porters in Abia with 10 deliveries14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text:
+        "14 Porters in Abia with 10 deliveries14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    },
+    {
+      name: "Abia",
+      text: "14 Porters in Abia with 10 deliveries"
+    }
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
